59f9e9c69767ac74e7441880ed670ce4c20053e2
yainhue <huentoyain4iaa@gmail.com>
2026-03-23 23:39:06 -0300

    W03 Chamber Page fixes

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -1,8 +1,10 @@
 const hamButton = document.querySelector(".hamburger-icon");
 const navigation = document.querySelector(".nav-list");
+const nav = document.querySelector("#base-nav");
 
 hamButton.addEventListener("click", () => {
     navigation.classList.toggle("nav-list-open");
+    nav.classList.toggle("nav-open");
     hamButton.classList.toggle("hamburger-icon-open");
     // navImg.classList.toggle("nav-img-open");
 });
