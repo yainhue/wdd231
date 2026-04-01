9a5b91e6b447629925134be77f0c1966e8ab9806
yainhue <huentoyain4iaa@gmail.com>
2026-04-01 16:52:25 -0300

    W05: Chamber Discover Page fixes

--- chamber/scripts/visit.js
+++ chamber/scripts/visit.js
@@ -23,31 +23,3 @@ else {
     welcomeMsg.textContent = `You last visited ${daysDifference.toFixed(0)} ${daysText} ago`;
     localStorage.setItem("last-visit-ts", dateNow)
 }
-
-
-
-
-
-
-
-// contenido aca para q haga la cosa insana
-
-// milliseconds to days constant = 1000 ms/s * 60 s/m * 60 m/h * 24 h/day
-
-// today's date
-// const theDateToday = new Date();
-
-// // processing
-// // const today = Date.now();
-// const christmasDate = new Date(Date.UTC(theDateToday.getFullYear(), 11, 25));
-// // check if is the waing days of December, if so ... change to next year.
-// if (theDateToday.getMonth() == 11 && theDateToday.getDate() > 25) {
-//     christmasDate.setFullYear(christmasDate.getFullYear() + 1);
-// }
-// // find difference between epoch times in ms and convert to days
-// let daysleft = (christmasDate.getTime() - Date.now()) / msToDays;
-
-// todayElement.textContent = today;
-// christmasElement.textContent = christmasDate.getTime();
-// christmasDateElement.textContent = christmasDate;
-// daysElement.textContent = `${daysleft.toFixed(0)} days`;
